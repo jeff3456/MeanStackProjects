9e66256abe5466a29e9de1da5be2c7b671ff393b
jeff3456 <jeff3456@gmail.com>
2017-03-24 18:27:58 -0400

    finished thinkster tutorial

--- thinkster tutorial/app.js
+++ thinkster tutorial/app.js
@@ -1,27 +1,51 @@
-var app = angular.module('flapperNews',[]);
+var app = angular.module('flapperNews',['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function(){
+    // service body
+    var o = {
+        posts: []
+    };
+    return o;
+}]);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
-    $scope.test = 'Hello world!';
-    $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 10},
-        {title: 'post 4', upvotes: 23},
-        {title: 'post 5', upvotes: 4}
-    ];
-    $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1;
-    };
-    $scope.addPost = function(){
-        if(!$scope.title || $scope.title === '') { return; }
+    '$scope',
+    'posts',
+    function($scope, posts){
+        $scope.test = 'Hello world!';
+        $scope.posts = posts.posts;
+        $scope.posts = [
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 10},
+            {title: 'post 4', upvotes: 23},
+            {title: 'post 5', upvotes: 4}
+        ];
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
         $scope.posts.push({
             title: $scope.title,
             link: $scope.link,
             upvotes: 0
         });
-        $scope.title = '';
-        $scope.link = '';
-    };
+            $scope.title = '';
+            $scope.link = '';
+        };
 }]);
